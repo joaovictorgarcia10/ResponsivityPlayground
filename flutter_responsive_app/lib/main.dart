c1dab31153e020317871e48a98d0778541180379
João Victor Garcia <garcialeite110@hotmail.com>
2022-07-07 12:22:29 -0300

    changes

--- flutter_responsive_app/lib/main.dart
+++ flutter_responsive_app/lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Responsive App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: const HomePage(),
     );
   }
